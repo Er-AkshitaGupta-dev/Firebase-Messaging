00b89d70382b821f0ad4de2dc5b0cbb844637868
Akshita <er.akshitagupta@gmail.com>
2021-05-26 13:42:07 +0545

    Initial commit

---
+++ lib/model/message.dart
@@ -0,0 +1,14 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+
+@immutable
+class Message {
+  final String title;
+  final String body;
+
+  const Message({
+    @required this.title,
+    @required this.body,
+  });
+}
---
+++ lib/widget/messaging_widget.dart
@@ -0,0 +1,65 @@
+// @dart=2.9
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_notification/model/message.dart';
+
+class MessagingWidget extends StatefulWidget {
+  @override
+  _MessagingWidgetState createState() => _MessagingWidgetState();
+}
+
+class _MessagingWidgetState extends State<MessagingWidget> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final List<Message> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        final notification = message['notification'];
+        setState(() {
+          messages.add(Message(
+              title: notification['title'], body: notification['body']));
+        });
+      },
+      onMessageOpenedApp: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+
+        final notification = message['data'];
+        setState(() {
+          messages.add(Message(
+            title: '${notification['title']}',
+            body: '${notification['body']}',
+          ));
+        });
+      },
+      onMessageOpenedApp: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   RemoteNotification notification = message.notification;
+    //   AndroidNotification android = message.notification?.android;
+    //   setState(() {
+    //     messages
+    //         .add(Message(title: notification.title, body: notification.body));
+    //   });
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) => ListView(
+        children: messages.map(buildMessage).toList(),
+      );
+
+  Widget buildMessage(Message message) => ListTile(
+        title: Text(message.title),
+        subtitle: Text(message.body),
+      );
+}
